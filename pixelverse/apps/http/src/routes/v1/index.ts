324536605dab6ed56af71de5fc051eb475634cce
owner-vst <owner.reliance@gmail.com>
2026-05-10 02:58:06 +0530

    Backend API's skeleton

--- pixelverse/apps/http/src/routes/v1/index.ts
+++ pixelverse/apps/http/src/routes/v1/index.ts
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import { userRouter } from "./user";
+import { spaceRouter } from "./space";
+import { adminRouter } from "./admin";
 
 export const router= Router();
 
@@ -13,3 +16,18 @@ router.get("/signin", (req, res)=>{
         message: "Signin successful"
     });
 })
+
+router.get("/elements", (req, res)=>{
+    res.json({
+        message: "Elements retrieved successfully"
+    });
+})
+
+router.get("/avatars",(req,res)=>{
+    res.json({
+        
+    })
+})
+router.use("/user",userRouter);
+router.use("/space",spaceRouter);
+router.use("/admin",adminRouter);
---
+++ pixelverse/apps/http/src/routes/v1/space.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+
+export const spaceRouter = Router();
+
+spaceRouter.post("/",(req,res)=>{
+
+})
+
+spaceRouter.get("/:spaceId",(req,res)=>{
+    
+})
+
+spaceRouter.delete("/:spaceId",(req,res)=>{
+    
+})
+
+spaceRouter.post("/all",(req,res)=>{
+    
+})
+
+spaceRouter.post("/element",(req,res)=>{
+
+})
+
+spaceRouter.delete("/element",(req,res)=>{
+    
+})
